38e06102676bb126751df2349c4944688e6ffa2e
anand raatna maurya <anandratna348@gmail.com>
2025-06-17 23:31:12 +0530

    database created and flask server-run

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,20 @@
-#one check
+from flask import Flask
+from config import Config
+from models import db
+from flask_migrate import Migrate
+
+
+# Initialize Flask app
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object(Config)
+
+# Initialize database
+db.init_app(app)
+migrate = Migrate(app, db)
+
+@app.route('/')
+def home():
+    return 'Hello Everyone'
+
+if __name__ == '__main__':
+    app.run(debug=True)
