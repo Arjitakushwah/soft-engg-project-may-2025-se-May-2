7824c032b3a0148181a026b9941cdb4e01c421a9
anand raatna maurya <anandratna348@gmail.com>
2025-06-18 18:12:14 +0530

    Parent register API, User Login usingJWT Authentication implimented

--- backend/config.py
+++ backend/config.py
@@ -6,3 +6,4 @@ INSTANCE_DIR = os.path.join(BASE_DIR, 'instance')
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(INSTANCE_DIR, 'project.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = 'SEM20257'
