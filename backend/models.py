437a36cdb7bac587de21970add8324e9d52717e7
anand raatna maurya <anandratna348@gmail.com>
2025-06-19 16:59:34 +0530

    Added Parent SignUP, Child and Parentlogin, and Child add API

--- backend/models.py
+++ backend/models.py
@@ -8,9 +8,10 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
-    role = db.Column(db.String, nullable=False)  # 'parent' or 'child'
+    role = db.Column(db.String, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
 # ---------- Parent Model ----------
