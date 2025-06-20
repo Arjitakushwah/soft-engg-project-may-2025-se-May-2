2e7363649bc6805fd74a41b30fe0b7edc440b323
Arjita Singh Kushwaha <arjitakushwah@gmail.com>
2025-06-21 01:01:54 +0530

    Migrated project to Vue CLI structure with App.vue, Home.vue, Navbar.vue

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Life Skills App</a>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Navbar',
+  };
+  </script>
+  
+  <style scoped>
+  /* You can add scoped navbar styles here */
+  </style>
+  
