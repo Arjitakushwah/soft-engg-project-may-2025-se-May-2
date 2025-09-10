cbc8909b0659ab45ae381206a0e4da8d647644fe
Anand Ratna Maurya <55905899+anandratna348@users.noreply.github.com>
2025-09-10 11:07:25 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,18 +1,18 @@
 import os
-from dotenv import load_dotenv
-load_dotenv(dotenv_path="prod.env")
 from datetime import timedelta
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-INSTANCE_DIR = os.path.join(BASE_DIR, 'instance')
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(INSTANCE_DIR, 'project.db')
+    db_url = os.getenv("DATABASE_URL", 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'instance', 'project.db'))
+    if db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = db_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    
     JWT_SECRET_KEY = os.getenv('SEM20257', 'another_secret_key')
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
 
 class DevelopmentConfig(Config):
-    DEBUG = True 
+    DEBUG = True
+ 
+
